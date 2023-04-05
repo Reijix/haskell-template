e216d600f209dd8e14a45414f78acac6fa8fa494
reijix <leon.vatthauer@fau.de>
2023-04-05 15:30:09 +0200

    Strip some stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Main.Utf8 qualified as Utf8
 
 {- |
  Main entry point.
@@ -9,6 +8,4 @@ import Main.Utf8 qualified as Utf8
 -}
 main :: IO ()
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+  print "Hello world"
